67fed95698835b530275906ce66b33c35b5938e2
Nestor Castillo <nestor.castillo@izea.com>
2014-06-18 21:43:23 -0400

    Ruby Dojo in class exercise

--- number_in_words.rb
+++ number_in_words.rb
@@ -1,2 +1,25 @@
 class NumberInWords
+
+
+  ONES = %w(zero one two three four five six seven eight nine)
+  TEENS = %w(ten eleven twelve thirteen fourteen fifteen sixteen
+    seventeen eighteen nineteen)
+  SECOND_DIGITS = %w(x x twenty thirty fourty fifty sixty seventy eighty ninety)
+
+
+  def number_in_words(number)
+    word = []
+
+    if number > 20 && number < 99
+      word << SECOND_DIGITS[number / 10]
+    end
+
+    if number > 9 && number < 20
+      return TEENS[number % 10]
+    end
+
+    first_digit = number % 10
+    word << ONES[first_digit]
+    word.join(" ")
+  end
 end
--- number_in_words_spec.rb
+++ number_in_words_spec.rb
@@ -1,8 +1,27 @@
 require './number_in_words.rb'
 
 describe NumberInWords do
-
-  it 'first test' do
+  before :each do
+    @converter = NumberInWords.new
   end
 
+  it 'should return one for 1' do
+    expect(@converter.number_in_words(1)).to eq "one"
+  end
+
+  it 'should return two for 2' do
+    expect(@converter.number_in_words(2)).to eq "two"
+  end
+
+  it 'should return ten for 10' do
+    expect(@converter.number_in_words(10)).to eq "ten"
+  end
+
+  it 'should return thirty five for 35' do
+    expect(@converter.number_in_words(35)).to eq "thirty five"
+  end
+
+  it 'should return thirty for 30' do
+    expect(@converter.number_in_words(30)).to eq "thirty"
+  end
 end
